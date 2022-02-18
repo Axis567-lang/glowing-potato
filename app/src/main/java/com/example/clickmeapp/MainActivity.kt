0055edbe0bcf49692558cf27446913b932372810
axis567-lang <carolbc.wr@gmail.com>
2022-02-18 00:02:38 -0600

    Aplicación correcta, la anterior no era ehe

---
+++ app/src/main/java/com/example/clickmeapp/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.clickmeapp
+
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.method.ScrollingMovementMethod
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+private val TAG = "MainActivity"
+private val TEXT = "TextContent"
+
+class MainActivity : AppCompatActivity() {
+
+    private var TextView: TextView? = null
+
+    private var numTimesClicked = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        Log.d(TAG, "onCreate")
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val userInput: EditText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val button: Button = findViewById<Button>(R.id.button)
+        val button2: Button = findViewById(R.id.button2)
+        TextView = findViewById<TextView>(R.id.tarea_view)
+
+        TextView?.text = ""
+
+        TextView?.movementMethod = ScrollingMovementMethod()
+
+        button.setOnClickListener(object :View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d(TAG, "onClick")
+                TextView?.append(userInput.text)
+                TextView?.append("\n")
+                //userInput.setText("")
+                userInput.text.clear()
+            }
+        })
+
+        button2.setOnClickListener(object :View.OnClickListener {
+            override fun onClick(p0: View?) {
+                TextView?.text = ""
+            }
+        })
+    }
+    //
+    override fun onStart() {
+        Log.d(TAG, "onStart")
+        super.onStart()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG, "onRestoreInstanceState")
+        super.onRestoreInstanceState(savedInstanceState)
+        TextView?.text = savedInstanceState.getString(TEXT, "")
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        Log.d(TAG, "onSaveInstanceState")
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putString(TEXT, TextView?.text.toString())
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop")
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "onRestart")
+        super.onRestart()
+    }
+
+    //
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        super.onDestroy()
+    }
+
+}
--- app/src/test/java/com/example/clickmeapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/clickmeapp/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.rss
+package com.example.clickmeapp
 
 import org.junit.Test
 
